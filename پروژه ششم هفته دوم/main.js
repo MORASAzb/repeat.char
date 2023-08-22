208d5972092eead6f9b8fb51c79c2644a84a3a16
morasa zadbagher sighalani <100466704+MORASAzb@users.noreply.github.com>
2023-08-22 16:27:57 +0330

    Add files via upload

---
+++ پروژه ششم هفته دوم/main.js
@@ -0,0 +1,15 @@
+const inputString = prompt("please enter your string : ");
+const repeatCount = parseInt(prompt("Please enter the number of repetitions : "));
+let repeatedString = "";
+
+if (!isNaN(repeatCount) && repeatCount > 0) {
+    for (let i = 0; i < inputString.length - 1; i++) {
+        for (let j = 0; j < repeatCount; j++) {
+            repeatedString += inputString[i];
+        }
+    }
+
+    console.log(`your repeated string is :  ${repeatedString}`);
+} else {
+    console.log("please enter a real number!!!");
+}
